f2cda999c20b256b0ca8e076c79020253a338e69
ardyflora <eripflo@gmail.com>
2016-09-22 00:29:26 -0400

    [task] Updating blank page into dashboard and updating charts and map

---
+++ startbootstrap-sb-admin-master/js/amCharts.js
@@ -0,0 +1,37 @@
+var chart = AmCharts.makeChart( "chartdiv", {
+	"type": "pie",
+	"angle": 20.7,
+	"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+	"depth3D": 15,
+	"titleField": "category",
+	"valueField": "column-1",
+	"theme": "light",
+	"minRadius":90,
+	"allLabels": [],
+	"balloon": {},
+	"legend": {
+		"enabled": true,
+		"align": "center",
+		"markerType": "circle"
+	},
+	"titles": [
+		{
+			"id": "Title-1",
+			"text": "VEHICLE STATUS"
+		}
+	],
+	"dataProvider": [
+		{
+			"category": "In Garage",
+			"column-1": "20"
+		},
+		{
+			"category": "Stalled",
+			"column-1": "10"
+		},
+		{
+			"category": "In transit",
+			"column-1": "100"
+		}
+	]
+} );
---
+++ startbootstrap-sb-admin-master/js/amCharts2.js
@@ -0,0 +1,38 @@
+var chart = AmCharts.makeChart( "chartdiv1", {
+	"type": "pie",
+	"angle": 20.7,
+	"balloonText": "[[title]]<br><span style='font-size:14px'><b>[[value]]</b> ([[percents]]%)</span>",
+	"depth3D": 15,
+	"titleField": "category",
+	"valueField": "column-1",
+	"theme": "light",
+	"minRadius":90,
+	"allLabels": [],
+	"balloon": {},
+	"legend": {
+		"enabled": true,
+		"align": "center",
+		"markerType": "circle",
+		"minradius":90
+	},
+	"titles": [
+		{
+			"id": "Title-1",
+			"text": "DELIVERY STATUS"
+		}
+	],
+	"dataProvider": [
+		{
+			"category": "Running Late",
+			"column-1": "35"
+		},
+		{
+			"category": "Ahead Of Time",
+			"column-1": "10"
+		},
+		{
+			"category": "On Time",
+			"column-1": "55"
+		}
+	]
+} );
