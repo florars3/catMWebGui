01bfc3a265ab196551a7ac2620f3f8f3792b0787
ardyflora <eripflo@gmail.com>
2016-10-18 22:35:25 -0400

    Cleaning up code, fixing identation, and also fixing images and coloring

--- js/amCharts3.js
+++ js/amCharts3.js
@@ -1,70 +1,57 @@
-var chart = AmCharts.makeChart("chartdiv2",
-				{
-					"type": "serial",
-					"categoryField": "category",
-					"angle": 30,
-					"depth3D": 30,
-					"startDuration": 1,
-					"theme": "light",
-					"categoryAxis": {
-						"gridPosition": "start",
-						"titleFontSize": 0
-					},
-					"trendLines": [],
-					"graphs": [
-						{
-							"balloonText": "[[title]] of [[category]]:[[value]]",
-							"fillAlphas": 1,
-							"id": "AmGraph-1",
-							"title": "Fuel Cost(Annually)",
-							"type": "column",
-							"valueField": "column-1"
-						},
-						{
-							"balloonText": "[[title]] of [[category]]:[[value]]",
-							"fillAlphas": 1,
-							"id": "AmGraph-2",
-							"title": "Fuel Cost Reduced 33%(Annually)",
-							"type": "column",
-							"valueField": "column-2"
-						}
-					],
-					"guides": [],
-					"valueAxes": [
-						{
-							"id": "ValueAxis-1",
-							"title": "Fuel Cost(Annually)"
-						}
-					],
-					"allLabels": [],
-					"balloon": {},
-					"legend": {
-						"enabled": true,
-						"useGraphSettings": true
-					},
-					"titles": [
-						{
-							"id": "Title-1",
-							"size": 15,
-							"text": "Reduced $ by Controlling Aggressive Driving"
-						}
-					],
-					"dataProvider": [
-						{
-							"category": "2013",
-							"column-1": "100",
-							"column-2": "70"
-						},
-						{
-							"category": "2014",
-							"column-1": "60",
-							"column-2": "40"
-						},
-						{
-							"category": "2015",
-							"column-1": "50",
-							"column-2": "40"
-						}
-					]
-				}
-			);
+var chart = AmCharts.makeChart("chartdiv2", {
+    "type": "serial",
+    "categoryField": "category",
+    "angle": 30,
+    "depth3D": 30,
+    "startDuration": 1,
+    "theme": "light",
+    "categoryAxis": {
+        "gridPosition": "start",
+        "titleFontSize": 0
+    },
+    "trendLines": [],
+    "graphs": [{
+        "balloonText": "[[title]] of [[category]]:[[value]]",
+        "fillAlphas": 1,
+        "id": "AmGraph-1",
+        "title": "Fuel Cost(Annually)",
+        "type": "column",
+        "valueField": "column-1"
+    }, {
+        "balloonText": "[[title]] of [[category]]:[[value]]",
+        "fillAlphas": 1,
+        "id": "AmGraph-2",
+        "title": "Fuel Cost Reduced 33%(Annually)",
+        "type": "column",
+        "valueField": "column-2"
+    }],
+    "guides": [],
+    "valueAxes": [{
+        "id": "ValueAxis-1",
+        "title": "Fuel Cost(Annually)"
+    }],
+    "allLabels": [],
+    "balloon": {},
+    "legend": {
+        "enabled": true,
+        "useGraphSettings": true
+    },
+    "titles": [{
+        "id": "Title-1",
+        "size": 15,
+        "text": "Reduced $ by Controlling Aggressive Driving"
+    }],
+    "dataProvider": [{
+        "category": "2013",
+        "column-1": "100",
+        "column-2": "70"
+    }, {
+        "category": "2014",
+        "column-1": "60",
+        "column-2": "40"
+    }, {
+        "category": "2015",
+        "column-1": "50",
+        "column-2": "40"
+    }]
+});
--- startbootstrap-sb-admin-master/README.md
+++
@@ -1,5 +0,0 @@
-## Getting Started
-
-To begin using this template, choose one of the following options to get started:
-* Clone the repo: `git clone https://github.com/florars3/catMWebGui.git`
-* Fork the repo
